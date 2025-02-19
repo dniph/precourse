422e49f7125c5c578b24aac45553f35c3f231503
Dennif Portilla <dennif.pa@gmail.com>
2025-02-18 18:19:13 -0800

    story.py added

---
+++ story.py
@@ -0,0 +1,5 @@
+holiday = input("What is your favorite holliday ?")
+noun = input("Enter a noun")
+place = input("Enter a place")
+
+print(f"I can't believe it's already {holiday}! I can't wait to put on my {noun} and visit every {place} in my neighborhood.")
