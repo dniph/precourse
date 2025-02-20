3ee4f359b284961ed47e721aa284d2f6aa1f5586
Dennif <dennif.pa@gmail.com>
2025-02-19 16:11:17 -0800

    game.py added

---
+++ game.py
@@ -0,0 +1,22 @@
+import random 
+
+RANGE_LOW = 0
+RANGE_HIGH = 100
+random_number = (random.randint(RANGE_LOW,RANGE_HIGH))
+user_input_string = input("Guess the number: ")
+
+if user_input_string.isnumeric():
+    user_input = int(user_input_string)
+    
+    if user_input < RANGE_LOW or user_input > RANGE_HIGH:
+        print("Your Guess is out of the bounds")
+        print ("You must be between {RANGE_LOW} and {RANGE_HIGH}")
+    elif user_input_string == random_number:
+        print("Honey, you guessed the number, Good Job! ")
+    elif user_input > random_number:
+        print("Puu! Your guess is too high")
+    elif user_input < random_number:
+        print("Puu! Your guess is too low")
+    
+else:
+    print("You must input a number!")
