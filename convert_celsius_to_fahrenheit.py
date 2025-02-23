4aba75cf8bf85b6ff71fe8adcef8f81f2b8bee9f
Dennif <dennif.pa@gmail.com>
2025-02-22 22:07:20 -0800

    convert_celcius_to_fahrenheit added

---
+++ convert_celsius_to_fahrenheit.py
@@ -0,0 +1,11 @@
+def convert_celsius_to_fahrenheit(temp):
+    return 9 / 5 * temp + 32
+
+temp = input("Enter a number: ")
+temp = float(temp)
+result = convert_celsius_to_fahrenheit(temp)
+
+print(result) 
+
+
+
